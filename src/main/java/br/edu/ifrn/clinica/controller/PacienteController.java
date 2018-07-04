90f6f5f9024effc7bda9c06df7547cc67ef68425
João Roberto <joao.roberto@academico.ifrn.edu.br>
2018-07-04 00:58:35 -0300

    Correção End-Point Paciente

--- src/main/java/br/edu/ifrn/clinica/controller/PacienteController.java
+++ src/main/java/br/edu/ifrn/clinica/controller/PacienteController.java
@@ -1,31 +1,23 @@
 package br.edu.ifrn.clinica.controller;
 
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.Errors;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.edu.ifrn.clinica.dto.PacienteDTO;
-import br.edu.ifrn.clinica.model.Cidade;
-import br.edu.ifrn.clinica.model.Convenio;
 import br.edu.ifrn.clinica.model.Paciente;
-import br.edu.ifrn.clinica.model.enums.Sexo;
-import br.edu.ifrn.clinica.repository.CidadeRepository;
-import br.edu.ifrn.clinica.repository.ConvenioRepository;
-import br.edu.ifrn.clinica.repository.PacienteRepository;
 import br.edu.ifrn.clinica.services.PacienteService;
 
 
@@ -33,37 +25,14 @@ import br.edu.ifrn.clinica.services.PacienteService;
 @RequestMapping(value="/paciente")
 public class PacienteController {
 	
-	private static final String PACIENTE_VIEW = "Paciente/Paciente";
-	private static final String PACIENTE_CADASTRO_VIEW = "Paciente/PacienteCadastro";
-	
 	@Autowired
 	private PacienteService service;
 	
-	@Autowired
-	private PacienteRepository pacientes;
-	
-	@Autowired
-	private CidadeRepository cidades;
-	
-	@Autowired
-	private ConvenioRepository convenios;
-
-	@GetMapping("/")
-	public ModelAndView paciente() {
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<PacienteDTO>> findAll() {
 		List<Paciente> list = service.findAll();
 		List<PacienteDTO> listDto = list.stream().map(obj -> new PacienteDTO(obj)).collect(Collectors.toList());
-		ModelAndView mv = new ModelAndView(PACIENTE_VIEW);
-		mv.addObject("listaPacientes", pacientes.count());
-		mv.addObject("pacientes", listDto);
-		return mv;
-	}
-	
-	@RequestMapping("/novo")
-	@PreAuthorize("hasAnyRole('ADMIN')")
-	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView(PACIENTE_CADASTRO_VIEW);
-		mv.addObject(new Paciente());
-		return mv;
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
@@ -74,51 +43,18 @@ public class PacienteController {
 
 	@RequestMapping(method = RequestMethod.POST)
 	@PreAuthorize("hasAnyRole('ADMIN')")
-	public String salvar(@Validated Paciente paciente, Errors errors, RedirectAttributes attributes) {
-		if (errors.hasErrors()) {
-			return PACIENTE_CADASTRO_VIEW;
-		}
-		try {
-			service.salvar(paciente);
-			attributes.addFlashAttribute("mensagem", "Paciente Salvo com sucesso!");
-			return "redirect:/paciente/novo";
-		} catch (Exception e) {
-			errors.rejectValue("Paciente", null, e.getMessage());
-			return PACIENTE_CADASTRO_VIEW;
-		}
+	public ResponseEntity<Void> salvar(@Valid @RequestBody Paciente paciente) {
+		service.salvar(paciente);
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(paciente.getId()).toUri();
+			return ResponseEntity.created(uri).build();
 	}
 	
-	@RequestMapping(value="/editar/{id}")
 	@PreAuthorize("hasAnyRole('ADMIN')")
-	public ModelAndView edicao(@PathVariable("id") Paciente paciente) {
-		ModelAndView mv = new ModelAndView(PACIENTE_CADASTRO_VIEW); 
-		mv.addObject(paciente);
-		return mv;
-	}
-	
-	@RequestMapping(value="{id}", method = RequestMethod.DELETE)
-	@PreAuthorize("hasAnyRole('ADMIN')")
-	public String excluir(@PathVariable Long id, RedirectAttributes attributes) {
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> excluir(@PathVariable Long id) {
 		service.delete(id);
-		
-		attributes.addFlashAttribute("mensagem", "Paciente Excluido com sucesso!");
-		return "redirect:/paciente/";
+		return ResponseEntity.noContent().build();
 	}
-	
-	@ModelAttribute("listaCidades")
-	public List<Cidade> listaCidades() {
-		List<Cidade> list = cidades.findAll();
-		return list;
-	}	
-	
-	@ModelAttribute("listaSexo")
-	public List<Sexo> listaSexo() {
-		return Arrays.asList(Sexo.values());
-	}
-	
-	@ModelAttribute("listaConvenio")
-	public List<Convenio> listaConvenio() {
-		List<Convenio> list = convenios.findAll();
-		return list;
-	}		
+
 }
